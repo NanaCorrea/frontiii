1d13d0329886b3fab7dd61aa7a5b969d20947b2c
Mariana Correa <nana.filosofia@hotmail.com>
2023-02-27 19:50:50 -0300

    primeiro commit

---
+++ aula02/src/App.jsx
@@ -0,0 +1,19 @@
+// const App = ()=> {
+//   return <h1>Meu primeiro componente</h1>
+// }
+
+// const App = () => <h1>Meu primeiro componente</h1>;
+
+function App(){
+  return (
+    <main>
+      <h1>Meu primeiro componente</h1>
+      <p>Meu texto</p>
+      <ul>
+        <li>Item 1</li>
+      </ul>
+    </main>
+    )
+}
+
+export default App;
