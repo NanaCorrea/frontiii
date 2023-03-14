912d006f128a3c5dcd52f71f880a864d8555dcc3
Mariana Correa <nana.filosofia@hotmail.com>
2023-03-14 13:27:50 -0300

    Co-authored-by: GiMGalvao <GiMGalvao@users.noreply.github.com> Co-authored-by: Ricardo Freitas <ric.freitas@msn.com> Co-authored-by: Johnny Wesley <johnnywesley.contato@outlook.com> Co-authored-by: Josemar <Josemar11@users.noreply.github.com>

--- aula09/src/App.jsx
+++ aula09/src/App.jsx
@@ -14,23 +14,47 @@ import { useState } from 'react'
 // ]
 
 function App() {
+  const [id, setId] = useState("")
   const [disciplina, setDisciplina] = useState("")
   const [duracao, setDuracao] = useState("")
   const [listaDisciplinas, setListaDisciplinas] = useState([])
 
   function addItem(event) {
     event.preventDefault()
-    setListaDisciplinas([...listaDisciplinas, {
+    if(disciplina == "" || duracao == ""){
+      alert("Preencha todas as informações");
+      return
+    }
+
+    if(id){
+      const copiaListaDisciplinas = [...listaDisciplinas]
+      const index = copiaListaDisciplinas.findIndex((item)=> item.id === id)
+      copiaListaDisciplinas[index].disciplina = disciplina
+      copiaListaDisciplinas[index].duracao = duracao   
+    }else{
+      setListaDisciplinas([...listaDisciplinas, {
       id: Date.now(),
       disciplina:disciplina,
       duracao:duracao
     }])
+    }
+
     setDisciplina("")
     setDuracao("")
+    setId("")
   }
 
   function apagarItem(id) {
-    
+    if (confirm("Clique em OK para confirmar a exclusão")) {
+      const result = listaDisciplinas.filter((item)=> item.id !== id)
+      setListaDisciplinas(result)
+    }
+  }
+
+  function preencherEstados(item){
+    setDisciplina(item.disciplina)
+    setDuracao(item.duracao)
+    setId(item.id)
   }
 
   return (
@@ -50,7 +74,7 @@ function App() {
         </select>
         <br />
         <br />
-        <input type="submit" value="Cadastrar"/>
+        <input type="submit" value={id ? "Salvar" : "Cadastrar"}/>
       </form>
 
       {listaDisciplinas.length > 0 ? (
@@ -58,7 +82,7 @@ function App() {
         {listaDisciplinas.map((item)=>{
           const {id, disciplina, duracao} = item
           return(
-            <li key={id} style={{border: "1px solid black"}}><p>Nome da Disciplina: {disciplina}</p><p>Carga horária: {duracao}</p><button onClick={()=> apagarItem(item.id)} >Apagar</button></li>
+            <li key={id} style={{border: "1px solid black"}}><p>Nome da Disciplina: {disciplina}</p><p>Carga horária: {duracao}</p><button onClick={()=> apagarItem(item.id)} >Apagar</button><button onClick={()=> preencherEstados(item)} >Editar</button></li>
           )
           })}
       </ul>) : (
